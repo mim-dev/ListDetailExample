386278cd957c31d71ffb88fbd4d69b8f308aa1c9
Luther Stanton <luther.stanton@gmail.com>
2025-04-29 08:36:58 -0400

    adds basic project structure

---
+++ ListDetailExample/ListDetailExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListDetailExampleApp.swift
+//  ListDetailExample
+//
+//  Created by Luther Stanton on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ListDetailExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListDetailExampleTests/ListDetailExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListDetailExampleTests.swift
+//  ListDetailExampleTests
+//
+//  Created by Luther Stanton on 4/29/25.
+//
+
+import Testing
+@testable import ListDetailExample
+
+struct ListDetailExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
